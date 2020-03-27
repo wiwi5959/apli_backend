4a89add7c22c56932faa0c06f9811d7991cda44a
wiwi5959 <wiwi5959@outlook.fr>
2020-03-27 16:28:38 +0100

    wip

---
+++ projet_dot_net06/projet_dot_net/Models/Etudiant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class Etudiant
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }     
+    }
+}
